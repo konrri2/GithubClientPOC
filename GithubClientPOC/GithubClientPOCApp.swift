566e8b49c9790c506b3d19c75404ab687b7e6353
Konrad <konrri@gmail.com>
2024-01-26 19:45:04 +0100

    Add empty project and .gitignore

---
+++ GithubClientPOC/GithubClientPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubClientPOCApp.swift
+//  GithubClientPOC
+//
+//  Created by Konrad Leszczyński on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GithubClientPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
